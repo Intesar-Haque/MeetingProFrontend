71c1ea5a18f17eca5f9ae1517bf28c73273c9f1f
Intesar Haque <intesarhaque99@gmail.com>
2022-08-28 00:13:20 +0600

    Firebase

--- src/app/services/ApiEndpoint.ts
+++ src/app/services/ApiEndpoint.ts
@@ -1,15 +1,12 @@
 export default class ApiEndpoint {
-    // static SOCKET_ENDPOINT = 'localhost:3000';
+    static SOCKET_ENDPOINT = 'https://ds-meeting-socket.herokuapp.com';
     // static SOCKET_ENDPOINT = 'http://182.163.112.207:9001';
-    static SOCKET_ENDPOINT = 'https://stupid-corners-reply-182-163-112-194.loca.lt';
-    // static PEER_ENDPOINT = 'localhost:3001';
+    static PEER_ENDPOINT = 'http://localhost:3001'
     // static PEER_ENDPOINT = 'http://182.163.112.207:9002';
-    static PEER_ENDPOINT = 'https://hip-boxes-hammer-182-163-112-194.loca.lt';
-    // static SERVICE_ENDPOINT = 'http://localhost:8080';
-    // static SERVICE_ENDPOINT = 'http://182.163.112.207:9003';
-    static SERVICE_ENDPOINT = 'https://light-worlds-lie-182-163-112-194.loca.lt';
+    static SERVICE_ENDPOINT = 'http://localhost:8080';
+//     static SERVICE_ENDPOINT = 'http://182.163.112.207:9003';
     // static MY_ADDRESS = 'http://182.163.112.207:9004';
-    static MY_ADDRESS = 'https://5fc0-182-163-112-194.ap.ngrok.io';
+    static MY_ADDRESS = 'https://datasoft-meeting.firebaseapp.com';
 
     static TURN_URI = 'https://global.xirsys.net/_turn/MeetingPro';
     static TURN_AUTH = 'Basic ' + btoa('intesar:e5f7e820-1afb-11ed-ba6b-0242ac150002');
--- src/app/services/peer.service.ts
+++ src/app/services/peer.service.ts
@@ -72,7 +72,8 @@ export class PeerService {
   }
 
   private initPeer(config: any): void {
-    this.peer = new Peer(this.myPeerId, ApiEndpoint.PEER_ENDPOINT);
+//     this.peer = new Peer(this.myPeerId, ApiEndpoint.PEER_ENDPOINT);
+    this.peer = new Peer(this.myPeerId);
   }
 
 }
